a0b57683b1feec39221f27c4681b7a9786cc09ea
☆ stargen ☆ <appakling@mail.com>
2024-10-09 13:13:41 -0500

    Update indexscript.js?

--- data/indexscript.js
+++ data/indexscript.js
@@ -1,5 +1,5 @@
 function goToPage(e) {
-  let url="http://www.starg3n.github.io/"+e.target.value;  
+  let url="http://www.starg3n.github.io/"+e.";  
   console.log(url);
   window.location = url;
 }
